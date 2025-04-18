7664a853fa0727a93269ef41ad179d117a981898
cbneville <63681644+cbneville@users.noreply.github.com>
2025-04-18 16:27:20 -0400

    Create download_manual_pdfs.sh

---
+++ game_manuals/download_manual_pdfs.sh
@@ -0,0 +1,5 @@
+wget https://cdn.svc.asmodee.net/production-zman/uploads/2024/09/Pandemic_Rulebook.pdf
+wget https://www.riograndegames.com/wp-content/uploads/2019/08/Puerto-Rico-Deluxe-Rules.pdf
+wget https://ncdn0.daysofwonder.com/tickettoride/de/img/te_rules_2015_en.pdf
+wget https://cdn.1j1ju.com/medias/ae/89/37-mysterium-rulebook.pdf
+wget https://cdn.1j1ju.com/medias/c6/cd/89-everdell-rulebook.pdf
